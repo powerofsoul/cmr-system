74324650ff8e52c81cc7d6a6cc328eb3e6839039
Florin Munteanu <florinmunteanu96@gmail.com>
2018-09-30 10:45:15 +0300

    Reflect changes to anyone using the app
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

--- utils.php
+++ utils.php
@@ -1,8 +1,23 @@
 <?php
-$records = $_POST["records"];
-echo $records;
-$recordsFile = fopen("records.txt", "w") or die("Unable to open file!");
- 
-fwrite($recordsFile, $records);
-fclose($recordsFile);
+$requestType = $_POST["requestType"];
+if($requestType == "write"){
+    $records = $_POST["records"];
+    echo $records;
+    $recordsFile = fopen("records.json", "w") or die("Unable to open file!");
+    
+    fwrite($recordsFile, $records);
+    fclose($recordsFile);
+
+    echo date('D M d Y H:i:s O', filemtime("records.json"));
+}
+else if($requestType == "read"){
+    $file = fopen("records.json", "r") or die("Unable to open file!");
+    echo fread($file,filesize("records.json"));
+    fclose($file);
+}
+else if($requestType == "lastModified"){
+    if (file_exists("records.json")) {
+        echo date('D M d Y H:i:s O', filemtime("records.json"));
+    }
+}
 ?>
