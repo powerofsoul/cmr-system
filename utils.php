f98fcff73d037cfd6acd33fea69977b5a0f52b14
Florin Munteanu <florinmunteanu96@gmail.com>
2018-10-14 21:37:49 +0300

    Add ability to archive

--- utils.php
+++ utils.php
@@ -1,5 +1,8 @@
 <?php
-$requestType = $_GET["requestType"];
+if(isset($_GET["requestType"]))
+    $requestType = $_GET["requestType"];
+else
+    $requestType = $_POST["requestType"];
 if($requestType == "write"){
     $records = $_GET["records"];
     $recordsFile = fopen("records.json", "w") or die("Unable to open file!");
@@ -10,8 +13,8 @@ if($requestType == "write"){
     echo date('D M d Y H:i:s O', filemtime("records.json"));
 }
 else if($requestType == "read"){
-    $file = fopen("records.json", "r") or die("Unable to open file!");
-    echo fread($file,filesize("records.json"));
+    $file = fopen($_GET["file"], "r") or die("Unable to open file!");
+    echo fread($file,filesize($_GET["file"]));
     fclose($file);
 }
 else if($requestType == "lastModified"){
@@ -22,18 +25,31 @@ else if($requestType == "lastModified"){
     $tagFile = fopen("tags.json", "r") or die("Unable to open file!");
     $tagFileContent = fread($tagFile,filesize("tags.json"));
     echo $tagFileContent;
-}else if($requestType == "writeTag"){
-    $file = fopen("tags.json", "r") or die("Unable to open file!");
-    $tags =  fread($file, filesize("tags.json"));
-    fclose($file);
-
-    $json = json_decode($tags,true);  
-    $json[$_GET["tagName"]] = $_GET["tagValue"];
+}else if($requestType == "archiveTag"){
+    $previewTag = $_POST["previewTag"];
+    $items = $_POST["items"];
+    $id = $_POST["id"];
+    
+    if(file_exists("archive.json")){
+        $file = fopen("archive.json", "r") or die("Unable to open file!");
+        $archive =  fread($file, filesize("archive.json"));
+        fclose($file);
+    }else{
+        $archive = "[]";
+    }
+    $json = json_decode($archive,true);  
+    array_push($json, $previewTag);
     $json = json_encode($json);
-    
-    $file = fopen("tags.json", "w") or die("Unable to open file!");
-    
+    echo $json;
+    $file = fopen("archive.json", "w") or die("Unable to open file!");
+
     fwrite($file, $json);
     fclose($file);
+    if(!file_exists("archive"))
+        mkdir("archive");
+    $archiveItemFile = fopen("archive\\". $id .".json", "w");
+    
+    fwrite($archiveItemFile, json_encode($items));
+    fclose($archiveItemFile);
 }
 ?>
